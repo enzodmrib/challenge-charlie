64971e981478a0742ecb9b5a8ec5f06eccdd21cc
enzodmrib <enzorib2@gmail.com>
2022-02-09 09:07:35 -0300

    Adds a fetch with async and await

--- src/components/WeatherSearch/index.tsx
+++ src/components/WeatherSearch/index.tsx
@@ -9,25 +9,34 @@ const WeatherSearch: React.FC = () => {
   let [cityName, setCityName] = useState("");
   let [weatherData, setWeatherData] = useState(null);
   
-  console.log(cityName);
-
   const handleInputChange = (inputText: React.ChangeEvent<HTMLInputElement>) => {
     setCityName(inputText.target.value);
   }
 
-  const getWeatherInfo = async () => {
-    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=29e16138c1a63e3da37060cc1b72d0e0&units=metric`)
-    .then(response => response.json())
-    .then(data => setWeatherData(data));
-    console.log(weatherData)
+  const getWeatherData = async () => {
+    let data = await fetchWeatherData();
+    setWeatherData(data);
   }
+
+  const fetchWeatherData = async () => {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=29e16138c1a63e3da37060cc1b72d0e0&units=metric`);
+    let weatherInfo = await response.json();
+    return weatherInfo;
+  } 
+
+  // Done with promise
+  // const getWeatherInfo = () => {
+  //   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=29e16138c1a63e3da37060cc1b72d0e0&units=metric`)
+  //   .then(response => response.json())
+  //   .then(data => setWeatherData(data));
+  // }
 
   return (
   <Container>
     <SearchText>Search Weather for location</SearchText>
     <SearchBox>
         <SearchInput type="text" placeholder="Ex: Boston" onChange={handleInputChange}></SearchInput>
-        <SearchButton onClick={getWeatherInfo}>Search</SearchButton>
+        <SearchButton onClick={getWeatherData}>Search</SearchButton>
     </SearchBox>
   </Container>
   );
